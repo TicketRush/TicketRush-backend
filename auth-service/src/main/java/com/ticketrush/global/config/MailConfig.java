0daae4c902d42b4af70678a5a219db65fc0695d4
김혜림 <khrim981209@gmail.com>
2026-05-17 03:12:05 +0900

    [Refactor] #63 MailConfig 추가

---
+++ auth-service/src/main/java/com/ticketrush/global/config/MailConfig.java
@@ -0,0 +1,34 @@
+package com.ticketrush.global.config;
+
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.mail.autoconfigure.MailProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+@Profile("local")
+@RequiredArgsConstructor
+public class MailConfig {
+
+  private static final int DEFAULT_SMTP_PORT = 587;
+
+  private final MailProperties mailProperties;
+
+  @Bean
+  public JavaMailSender javaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+    mailSender.setHost(mailProperties.getHost());
+    mailSender.setPort(Objects.requireNonNullElse(mailProperties.getPort(), DEFAULT_SMTP_PORT));
+    mailSender.setUsername(mailProperties.getUsername());
+    mailSender.setPassword(mailProperties.getPassword());
+
+    mailSender.getJavaMailProperties().putAll(mailProperties.getProperties());
+
+    return mailSender;
+  }
+}
