fedc5ebf657c1c1c18087107414807cc03db511f
김혜림 <khrim981209@gmail.com>
2026-05-17 17:18:58 +0900

    [Refactor] #63 Profile(local) 제거

--- auth-service/src/main/java/com/ticketrush/global/config/MailConfig.java
+++ auth-service/src/main/java/com/ticketrush/global/config/MailConfig.java
@@ -5,12 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.mail.autoconfigure.MailProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
-@Profile("local")
 @RequiredArgsConstructor
 public class MailConfig {
 
