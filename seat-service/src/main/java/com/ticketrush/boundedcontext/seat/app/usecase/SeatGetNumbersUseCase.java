5ec070112a3ea276321b367832d7332907bdd169
50h33 <wneh2023@naver.com>
2026-05-23 16:01:31 +0900

    [Refactor] #37 좌석 ID 목록 순서대로 좌석 번호를 반환하도록 수정

--- seat-service/src/main/java/com/ticketrush/boundedcontext/seat/app/usecase/SeatGetNumbersUseCase.java
+++ seat-service/src/main/java/com/ticketrush/boundedcontext/seat/app/usecase/SeatGetNumbersUseCase.java
@@ -3,6 +3,10 @@ package com.ticketrush.boundedcontext.seat.app.usecase;
 import com.ticketrush.boundedcontext.seat.app.dto.response.SeatNumberResponse;
 import com.ticketrush.boundedcontext.seat.out.repository.SeatRepository;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +23,10 @@ public class SeatGetNumbersUseCase {
       return List.of();
     }
 
-    return seatRepository.findSeatNumbersByIdIn(seatIds);
+    Map<Long, SeatNumberResponse> seatNumbersById =
+        seatRepository.findSeatNumbersByIdIn(seatIds).stream()
+            .collect(Collectors.toMap(SeatNumberResponse::seatId, Function.identity()));
+
+    return seatIds.stream().map(seatNumbersById::get).filter(Objects::nonNull).toList();
   }
 }
