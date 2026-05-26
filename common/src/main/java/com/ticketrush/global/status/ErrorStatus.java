73fce10956e891afb7e12512a1cd481b07ccc7e5
민주's mac <calla20031@Mac.local>
2026-05-27 00:47:19 +0900

    [Chore] #222 spotless 포맷 정렬

--- common/src/main/java/com/ticketrush/global/status/ErrorStatus.java
+++ common/src/main/java/com/ticketrush/global/status/ErrorStatus.java
@@ -142,12 +142,10 @@ public enum ErrorStatus {
   PAYMENT_PROVIDER_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "PAYMENT_400_002", "지원하지 않는 결제 수단입니다."),
   PAYMENT_CARD_REJECTED(
       HttpStatus.BAD_REQUEST, "PAYMENT_400_003", "카드 결제가 거절되었습니다. 다른 카드로 시도해주세요."),
-  PAYMENT_LIMIT_EXCEEDED(
-      HttpStatus.BAD_REQUEST, "PAYMENT_400_004", "카드 한도 또는 결제 한도를 초과했습니다."),
+  PAYMENT_LIMIT_EXCEEDED(HttpStatus.BAD_REQUEST, "PAYMENT_400_004", "카드 한도 또는 결제 한도를 초과했습니다."),
 
   // Payment 404
-  PAYMENT_SESSION_NOT_FOUND(
-      HttpStatus.NOT_FOUND, "PAYMENT_404_001", "결제 세션이 만료되었거나 존재하지 않습니다."),
+  PAYMENT_SESSION_NOT_FOUND(HttpStatus.NOT_FOUND, "PAYMENT_404_001", "결제 세션이 만료되었거나 존재하지 않습니다."),
 
   // Payment 409
   PAYMENT_ALREADY_COMPLETED(HttpStatus.CONFLICT, "PAYMENT_409_001", "이미 결제가 완료된 예매입니다."),
