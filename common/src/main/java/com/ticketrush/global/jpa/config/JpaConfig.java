0682415880385a0d097d5c1efbf94ba0f9181fef
50h33 <wneh2023@naver.com>
2026-05-27 16:43:23 +0900

    [Refactor] #24 Clock Bean을 JPA Auditing에 연결

--- common/src/main/java/com/ticketrush/global/jpa/config/JpaConfig.java
+++ common/src/main/java/com/ticketrush/global/jpa/config/JpaConfig.java
@@ -2,20 +2,32 @@ package com.ticketrush.global.jpa.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 @RequiredArgsConstructor
 public class JpaConfig {
 
   private final EntityManager entityManager;
+  private final ObjectProvider<Clock> clockProvider;
 
   @Bean
   public JPAQueryFactory jpaQueryFactory() {
     return new JPAQueryFactory(entityManager);
+  }
+
+  @Bean
+  public DateTimeProvider auditingDateTimeProvider() {
+    Clock clock = clockProvider.getIfAvailable(Clock::systemDefaultZone);
+    return () -> Optional.of(LocalDateTime.now(clock));
   }
 }
